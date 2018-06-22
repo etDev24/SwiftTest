4b5cb0efc1a338d34223d50b9e9f1885dec212ee
Syed Qamar Abbas <et_dev24@TheEntertainerMe.com>
2018-06-22 15:41:51 +0500

    Initial Commit

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Syed Qamar Abbas on 6/22/18.
+//  Copyright © 2018 Syed Qamar Abbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
